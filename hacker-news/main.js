6df4f1148f1e05b85dd74cdfcf380065847c49db
lisa louison <lisa.louison@gmail.com>
2017-04-22 13:49:22 -0400

    added a tag to links

--- hacker-news/main.js
+++ hacker-news/main.js
@@ -11,25 +11,25 @@ console.log('what\'s new in Hackernews?');
     !!!HINT!!! maybe you need a list of some kind in your html... maybe
 */
 
-const news_url = 'https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty';
+const newsUrl = 'https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty';
 
-const article_url = 'https://hacker-news.firebaseio.com/v0/item/' 
+const articleBaseUrl = 'https://hacker-news.firebaseio.com/v0/item/' 
 
 // fetching the news_url, converting it to json format, then calling the locate titles function
 function getNewStories(){
-  fetch(news_url)
-    .then(function(response){
-      response.json()
-      .then(function(ids){
-        locateTitles(ids);
-      })
+  fetch(newsUrl)
+  .then(function(response){
+    response.json()
+    .then(function(ids){
+      locateTitles(ids);
+    })
   })
 }
 
 // creating a for of loop that fetches each article using the id, converting it to json format, then calling the displaytitle function
 function locateTitles(ids){
   for (let id of ids) {
-    fetch(`${article_url}${id}.json?print=pretty`)
+    fetch(`${articleBaseUrl}${id}.json?print=pretty`)
     .then(function(response){
       response.json()
       .then(function(article){
@@ -41,7 +41,7 @@ function locateTitles(ids){
 
 // for each article, creating a new list item who's innerhtml is the title property in the json object and appending it to the storydiv to be displayed on the dom. Calling the displayinfo function on each title
 function displayTitle(article){
-  console.log(article);
+  // console.log(article);
   const storyDiv = document.getElementById('story_titles');
   const title = document.createElement('li');
   title.innerHTML = article.title;
@@ -52,15 +52,16 @@ function displayTitle(article){
 // adding an event listener to each title that when clicked, displays the info and changes the color of the title 
 function displayInfo(title, article){
   title.addEventListener('click', function() {
-    console.log('I was clicked');
+    // locating the elements in the document to add the article info to
     const articleTitle = document.getElementById('article_title');
     const articleScore = document.getElementById('article_score');
     const articleAuthor = document.getElementById('article_author');
     const articleLink = document.getElementById('article_link');
+    // changing the innerhtml of each element with the article properties
     articleTitle.innerHTML = `Title: ${article.title}`;
     articleScore.innerHTML = `Score: ${article.score}`;
     articleAuthor.innerHTML = `Added by: ${article.by}`;
-    articleLink.innerHTML = `Read this story here: ${article.url}`;
+    articleLink.innerHTML = `Read this story here: <a href="${article.url}" target="_blank">${article.url}</a>`;
     title.style.color = '#828282';
   })
 }
