824a6abd3ee9978c6c413c604b9957984b087a7e
lisa louison <lisa.louison@gmail.com>
2017-04-22 13:24:42 -0400

    displaying article information

--- hacker-news/main.js
+++ hacker-news/main.js
@@ -15,8 +15,6 @@ const news_url = 'https://hacker-news.firebaseio.com/v0/newstories.json?print=pr
 
 const article_url = 'https://hacker-news.firebaseio.com/v0/item/' 
 
-const storyDiv = document.getElementById('story_titles');
-
 // fetching the news_url, converting it to json format, then calling the locate titles function
 function getNewStories(){
   fetch(news_url)
@@ -43,17 +41,26 @@ function locateTitles(ids){
 
 // for each article, creating a new list item who's innerhtml is the title property in the json object and appending it to the storydiv to be displayed on the dom. Calling the displayinfo function on each title
 function displayTitle(article){
-  // console.log(article);
+  console.log(article);
+  const storyDiv = document.getElementById('story_titles');
   const title = document.createElement('li');
   title.innerHTML = article.title;
   storyDiv.appendChild(title);
-  displayInfo(title);
+  displayInfo(title, article);
 }
 
 // adding an event listener to each title that when clicked, displays the info and changes the color of the title 
-function displayInfo(title){
+function displayInfo(title, article){
   title.addEventListener('click', function() {
     console.log('I was clicked');
+    const articleTitle = document.getElementById('article_title');
+    const articleScore = document.getElementById('article_score');
+    const articleAuthor = document.getElementById('article_author');
+    const articleLink = document.getElementById('article_link');
+    articleTitle.innerHTML = `Title: ${article.title}`;
+    articleScore.innerHTML = `Score: ${article.score}`;
+    articleAuthor.innerHTML = `Added by: ${article.by}`;
+    articleLink.innerHTML = `Read this story here: ${article.url}`;
     title.style.color = '#828282';
   })
 }
