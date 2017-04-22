031868513e0ff8a8ff58968bed7b4e3a756b8398
lisa louison <lisa.louison@gmail.com>
2017-04-22 11:54:12 -0400

    connected to api

--- hacker-news/main.js
+++ hacker-news/main.js
@@ -1,7 +1,6 @@
-console.log('main.js connected~');
+console.log('what\'s new in Hackernews?');
 
 /*
-
 1. When the page loads
   - make a fetch request to get the top 500 newest articles on hackernews
      !!!HINT!!! READ THE DOCUMENTATION! https://github.com/HackerNews/API
@@ -10,5 +9,23 @@ console.log('main.js connected~');
   - make a fetch call for each item
   - append each item's information to the list of articles
     !!!HINT!!! maybe you need a list of some kind in your html... maybe
-
 */
+
+const news_url = 'https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty';
+
+
+
+function getNewArticles(){
+  fetch(news_url)
+    .then(function(response){
+      response.json()
+      .then(function(news){
+        console.log(news);
+      })
+  })
+}
+
+
+window.onload = function() {
+    getNewArticles();
+}
